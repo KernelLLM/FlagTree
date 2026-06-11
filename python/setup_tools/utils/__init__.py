78cd36dd0649293b7c9dd389c83c64deeb00cd1d
zhzhcookie <zhengyang@baai.ac.cn>
2026-06-11 01:38:31 +0000

    [Tsingmicro] Fix build and test flir

--- python/setup_tools/utils/__init__.py
+++ python/setup_tools/utils/__init__.py
@@ -10,7 +10,8 @@ flagtree_submodules = {
                  commit_id="5842469a16b261e45a2c67fbfc308057622b03ee",
                  dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "triton_shared")),
     "flir":
-    tools.Module(name="flir", url="https://github.com/FlagTree/flir.git",
+    tools.Module(name="flir", url="https://github.com/tsingmicro-public-e/flir.git",
+                 commit_id="bfcf3a72b956ad8229b4c345a46ea629885b17e5",  # branch: feature/tsingmicro-sync
                  dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "flir")),
 }
 
